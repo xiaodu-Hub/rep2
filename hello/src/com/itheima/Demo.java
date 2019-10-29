dd3e73be8862165608eac8d3e386096408fdb48d
zhang <zhang@itcast.cn>
2019-10-29 17:09:05 +0800

    Demo第一次提交

--- hello/src/com/itheima/Demo.java
+++ hello/src/com/itheima/Demo.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("你的头发还好吗?");
+        System.out.println("我的头发很好啊, 你呢?");
     }
 }
