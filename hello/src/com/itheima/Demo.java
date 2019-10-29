06e065b83f508e602b4a77cb81fc43b2c7724fec
zhang <zhang@itcast.cn>
2019-10-29 17:11:35 +0800

    Demo第3次提交

--- hello/src/com/itheima/Demo.java
+++ hello/src/com/itheima/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("你的头发还好吗?");
         System.out.println("我的头发很好啊, 你呢?");
+        System.out.println("那就好啊， 不错的");
     }
 }
